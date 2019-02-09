a867dd357aa34ff28462f7589065ad8811f02422
NGUYEN CANH KHANH TRINH <trinh.nguyen@thecoraledge.com>
2019-02-10 00:01:53 +0700

    Add raw body

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
   var data = "";
   req.on('data', function (chunk) { data += chunk })
   req.on('end', function () {
-    req.rawBody = data;
+    req.rawData = data;
     next();
   })
 });
